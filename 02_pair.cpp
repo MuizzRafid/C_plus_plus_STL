7035d6e219a12383eb0d06914a2eb0f7778cdae6
Muizz Rafid <muizzrafid1@gmail.com>
2026-09-12 22:06:19 +0600

    pair of vector added

--- 02_pair.cpp
+++ 02_pair.cpp
@@ -2,6 +2,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void printvec(vector<pair<int, int>> &v)
+{
+	cout << "size of v :" << v.size() << endl;
+	for (int i = 0; i < v.size(); i++)
+	{
+		cout << v[i].first << " " << v[i].second << endl;
+	}
+	cout << endl;
+}
+
+int main()
+{
+	vector<pair<int, int>> v = {{1, 2}, {2, 3}, {3, 4}};
+	printvec(v);
+}
+
 int main()
 {
 	// pair usually come with 2 which is pair
@@ -45,6 +61,18 @@ int main()
 	{
 		cout << p_array[i].first << " " << p_array[i].second << endl;
 	}
+
+	//                        Pair In Vector
+	vector<pair<int, int>> v;
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		int x, y;
+		cin >> x >> y;
+		v.push_back(make_pair(x, y)); // or v.push_back({x,y}) //taking input of vector pair
+	}
+	printvec(v);
 
 	return 0;
 }
